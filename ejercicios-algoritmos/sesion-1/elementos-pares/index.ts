731ec1179aeb87bb1ebba64969bd5567372c61e4
NinaSol <ninamarotta18@gmail.com>
2024-08-13 23:37:13 -0300

    resuelvo ejercicio de elementos pares

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
@@ -1,4 +1,18 @@
-export default function filtrarPares(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+export default function filtrarPares(array: number[]): number[] {
+  const pairs: number[] = [];
+  const pairsCount: Map<number, number> = new Map();
+
+  // Count occurrences
+  array.forEach(number => {
+    pairsCount.set(number, (pairsCount.get(number) ?? 0) + 1);
+  });
+
+  // Filter out numbers with even occurrences
+  pairsCount.forEach((value: number, key: number) => {
+    if (value % 2 === 0) {
+      pairs.push(key);
+    }
+  });
+
+  return pairs;
 }
